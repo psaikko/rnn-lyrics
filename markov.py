c591d56985e51a94ce2ab35ac7ed2bb7f6fbb4d0
Paul Saikko <paul.saikko@gmail.com>
2020-05-10 18:18:40 +0300

    add command line arguments

--- markov.py
+++ markov.py
@@ -1,26 +1,40 @@
 import pandas as pd
 import numpy as np
 import random as rand
+import sys
 import os
 
-lyrics_df = pd.read_csv("lyrics.csv")
+data_filepath = "lyrics.csv"
 
-print(lyrics_df.columns)
-print(lyrics_df["genre"].unique())
-print(len(lyrics_df))
+if not os.path.exists(data_filepath):
+    print("Missing dataset:",data_filepath)
+    exit(1)
+lyrics_df = pd.read_csv(data_filepath)
 
-GENRE = "Hip-Hop"
+if len(sys.argv) < 3:
+    print("Missing required params!")
+    print(sys.argv[0] + "[GENRE] [M]")
+    print("GENRE: one of")
+    print(lyrics_df["genre"].unique())
+    print("M: number of state transition matrix dimensions")
+    exit(1)
+
+GENRE = sys.argv[1]
+M = int(sys.argv[2])
+
+if GENRE not in lyrics_df["genre"].unique():
+    print("Unrecognized genre", GENRE)
+    exit(1)
 
 genre_df = lyrics_df[lyrics_df.genre == GENRE]
 genre_df = genre_df.dropna(subset=["lyrics"])
-
 genre_lyrics = genre_df.lyrics.values
 
 print("Songs", len(genre_lyrics))
 print("Characters", sum(map(len, genre_lyrics)))
 
 alpha = list(chr(i) for i in range(ord('a'),ord('z')+1))
-nums = list(str(i) for i in range(0,10));
+nums = list(str(i) for i in range(0,10))
 punct = list(" ,.!?()\n")
 START_SYMBOL = "^"
 END_SYMBOL = "$"
@@ -38,11 +52,11 @@ def preprocess(lyric):
 def postprocess(indices):
     return ''.join(id_to_char[i] for i in indices)
 
-M = 3
 n_chars = len(allowed_chars)
 
 filename = f"{GENRE}-{M}.npy"
 
+# load or compute state-transition matrix from dataset
 if os.path.exists(filename):
     occ_mat = np.load(open(filename, "rb"))
 else:
@@ -56,9 +70,9 @@ else:
         occ_mat[ixs] += 1
     np.save(open(filename, "wb"), occ_mat)
 
-print(np.sum(occ_mat))
-
+# sample a song from the markov chain
 ixs = (START_ID,) * (M-1)
+song = []
 while True:
     r = rand.random()
     row = occ_mat[ixs]
@@ -73,5 +87,6 @@ while True:
 
     ixs = ixs[1:] + (next_i,)
     if next_i == END_ID: break
-    print(id_to_char[next_i],end='')
-print()
+    song += [next_i]
+
+print(postprocess(song))
