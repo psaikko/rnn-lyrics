e39672214d4e4677457f36a6a9fdae1b593907ea
Paul Saikko <paul.saikko@gmail.com>
2020-06-03 09:00:12 +0300

    sample from the model output for text

--- web/main.js
+++ web/main.js
@@ -1,10 +1,15 @@
 const num_classes = 256
+const temperature = 0.3
 const ord = c => c.charCodeAt(0);
 const chr = i => String.fromCharCode(i);
 
 // js argmax implementation https://gist.github.com/engelen/fbce4476c9e68c52ff7e5c2da5c24a28
 function from_onehot(array) {
     return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+function sample_from_logits(logits) {
+    return tf.multinomial(logits.map(x => x/temperature), 1).arraySync()[0]
 }
 
 const outputDiv = document.getElementById("outputDiv");
@@ -21,7 +26,8 @@ async function run_demo(model) {
         // Expand batch dimension and predict
         let res = model.predict(input.expandDims(0));
         let output = await res.array();
-        let out_chr = chr(from_onehot(output[0]));
+
+        let out_chr = chr(sample_from_logits(output[0]));
         
         s.push(out_chr);
         outputDiv.innerText = s.join("");
